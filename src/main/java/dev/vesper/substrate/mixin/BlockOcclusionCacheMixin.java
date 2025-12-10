b4b7df0db5a5e755170bbab8c03601d9369a6b7b
VesMaybeVesper <vesmaybevesper@gmail.com>
2025-12-09 20:08:17 -0500

    add sodium mixin

---
+++ src/main/java/dev/vesper/substrate/mixin/BlockOcclusionCacheMixin.java
@@ -0,0 +1,29 @@
+package dev.vesper.substrate.mixin;
+
+import dev.vesper.substrate.Substrate;
+import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockOcclusionCache;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Contract;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = BlockOcclusionCache.class, remap = false)
+public abstract class BlockOcclusionCacheMixin {
+
+	@Contract(value = "-> fail", pure = true)
+	private BlockOcclusionCacheMixin() {
+		throw new AssertionError("No instances.");
+	}
+
+	@Inject(method = "shouldDrawSide", at = @At("HEAD"), cancellable = true)
+	private void shouldDrawSide(BlockState selfState, BlockGetter view, BlockPos selfPos, Direction facing, CallbackInfoReturnable<Boolean> cir){
+		if (Substrate.shouldRender(selfPos, facing)) return;
+
+		cir.setReturnValue(false);
+	}
+}
