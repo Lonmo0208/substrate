cd2b3fe5901118cc1710a65724b6bd062029ce29
VesMaybeVesper <vesmaybevesper@gmail.com>
2025-12-19 14:51:00 -0500

    prevent handle end tick from being called on server side (#12)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ stonecutter {
 		fun match(version: String, vararg loaders: String) =
 			loaders.forEach { version("$version-$it", version).buildscript = "build.$it.gradle.kts" }
 
-		match("1.21.11", "fabric", "neoforge")
+		match("1.21.11", "fabric")
 		match("1.21.9", "fabric", "neoforge")
 		match("1.21.1", "fabric", "neoforge")
 
